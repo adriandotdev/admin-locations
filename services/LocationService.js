1792248bd22ab132af8613422cdd9f8798acd5df
Adrian <marcelonads@gmail.com>
2024-04-16 08:52:08 +0800

    docs: UPDATED README.md
    docs: ADDED comments to LocationService.js

--- services/LocationService.js
+++ services/LocationService.js
@@ -12,6 +12,14 @@ module.exports = class LocationService {
 		this.#repository = new LocationRepository();
 	}
 
+	/**
+	 * Retrieve all of locations.
+	 *
+	 * @param {Object} payload
+	 * @param {payload.limit} limit Number of objects to returned.
+	 * @param {payload.offset} offset Starting object
+	 * @returns {Array}
+	 */
 	async GetLocations({ limit, offset }) {
 		if (typeof limit !== "number")
 			throw new HttpBadRequest(
@@ -42,21 +50,34 @@ module.exports = class LocationService {
 		parking_types,
 		parking_restrictions,
 	}) {
+		// Request to Google Geocoding API for the data based on the address provided.
 		const geocodedAddress = await axios.get(
 			`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
 				address
 			)}&key=${process.env.GOOGLE_GEO_API_KEY}`
 		);
 
+		/**
+		 * Get the address_components object
+		 *
+		 * This object contains all of the details related to a address such as municipality, region, and postal code.
+		 */
 		const address_components =
 			geocodedAddress.data.results[0]?.address_components;
 
+		// If location is not found.
 		if (!address_components) throw new HttpBadRequest("LOCATION_NOT_FOUND", []);
 
+		// Get the city name when the type is 'locality'
 		const city = address_components.find((component) =>
 			component.types.includes("locality")
 		)?.long_name;
 
+		/**
+		 * Get the region name when the type is 'administrative_area_level_1'
+		 *
+		 * Get the first three letters of the region, convert it to uppercase, and trim it.
+		 */
 		const region = String(
 			address_components.find((component) =>
 				component.types.includes("administrative_area_level_1")
@@ -66,36 +87,20 @@ module.exports = class LocationService {
 			.toUpperCase()
 			.trim();
 
+		/**
+		 * Get the postal code of the address when the type is 'postal_code'
+		 */
 		const postal_code = address_components.find((component) =>
 			component.types.includes("postal_code")
 		)?.long_name;
 
+		// Get the latitude, and longitude of the address
 		const { lat, lng } = geocodedAddress.data.results[0].geometry.location;
 
+		// Get the formatted address.
 		const formatted_address = geocodedAddress.data.results[0].formatted_address;
-		/**
-		 * @TODO
-		 * Fix this logic to get the postal code based on the address provided.
-		 * Must retrieve the administrative_level_5
-		 */
-		// const placeID = await axios.get(
-		// 	`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${"14.238077"}%2C${"121.1569948"}&radius=2000&type=post_office&key=${
-		// 		process.env.GOOGLE_GEO_API_KEY
-		// 	}`
-		// );
 
-		// const postal = await axios.get(
-		// 	`https://maps.googleapis.com/maps/api/geocode/json?place_id=${"ChIJ9bBUdhRivTMRBYrSM2j6qvo"}&key=${
-		// 		process.env.GOOGLE_GEO_API_KEY
-		// 	}`
-		// );
-
-		// logger.info({
-		// 	GEOCODE_ADDRESS_DATA: {
-		// 		data: geocodedAddress.data,
-		// 	},
-		// });
-
+		// Location Registration
 		try {
 			const result = await this.#repository.RegisterLocation({
 				cpo_owner_id: cpo_owner_id || null,
@@ -108,23 +113,28 @@ module.exports = class LocationService {
 				postal_code: postal_code || null,
 			});
 
+			const LOCATION_ID = result.insertId;
+
+			// Add location's facilities
 			const newFacilities = facilities.map((facility) => [
 				facility,
-				result.insertId,
+				LOCATION_ID,
 			]);
 
 			await this.#repository.AddLocationFacilities(newFacilities);
 
+			// Add location's parking types
 			const newParkingTypes = parking_types.map((parkingType) => [
 				parkingType.id,
-				result.insertId,
+				LOCATION_ID,
 				parkingType.tag,
 			]);
 
 			await this.#repository.AddLocationParkingTypes(newParkingTypes);
 
+			// Add location's parking restrictions
 			const newParkingRestrictions = parking_restrictions.map(
-				(parking_restriction) => [parking_restriction, result.insertId]
+				(parking_restriction) => [parking_restriction, LOCATION_ID]
 			);
 
 			const parkingRestrictionsResult =
@@ -145,12 +155,25 @@ module.exports = class LocationService {
 		}
 	}
 
+	/**
+	 * Retrieve all of the binded locations.
+	 *
+	 * @param {Number} cpoOwnerID Charging Point Operator's ID
+	 * @returns {Array}
+	 */
 	async GetBindedLocations(cpoOwnerID) {
 		const result = await this.#repository.GetBindedLocations(cpoOwnerID);
 
 		return result;
 	}
 
+	/**
+	 * Binds a location to CPO
+	 *
+	 * @param {Number} cpoOwnerID - Charging Point Operator's ID
+	 * @param {Number} locationID - Location's ID
+	 * @returns {Object}
+	 */
 	async BindLocation(cpoOwnerID, locationID) {
 		const result = await this.#repository.BindLocation(cpoOwnerID, locationID);
 
@@ -161,6 +184,13 @@ module.exports = class LocationService {
 		return status;
 	}
 
+	/**
+	 * Unbinds a location from CPO
+	 *
+	 * @param {Number} cpoOwnerID - Charging Point Operator's ID
+	 * @param {Number} locationID - Location's ID
+	 * @returns {Object}
+	 */
 	async UnbindLocation(cpoOwnerID, locationID) {
 		const result = await this.#repository.UnbindLocation(
 			cpoOwnerID,
@@ -174,6 +204,12 @@ module.exports = class LocationService {
 		return status;
 	}
 
+	/**
+	 * Retrieve all of the default data from database
+	 * for registration purposes.
+	 *
+	 * @returns {Object}
+	 */
 	async GetDefaultData() {
 		const facilities = await this.#repository.GetDefaultFacilities();
 		const parking_types = await this.#repository.GetDefaultParkingTypes();
